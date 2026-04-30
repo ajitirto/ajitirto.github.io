e2b1b59ae8ac6b40c6b11f0d344d9d509e60e364
ajitirto <ajiwoke@gmail.com>
2026-04-30 20:39:14 +0700

    update

--- antigravity/astro/astro.config.mjs
+++ antigravity/astro/astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://ajitirto.github.io',
-  base: '/antigravity/astro',
+  // base: '/antigravity/astro',
+  base: isGitHubPages ? '/antigravity/astro' : '/',
 });
